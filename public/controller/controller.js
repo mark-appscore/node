e56f0ec44290ab9daf6c517e948050993d0ad2da
mark-appscore <mark.wang@appscore.com.au>
2015-06-02 22:33:47 +1000

    request and response

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,23 +1,10 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
-    person1 = {
-    	name: 	'Tim',
-    	email: 	'tim@gmail.com',
-    	number: '(111) 111-1111'
-    };
-    person2 = {
-    	name: 	'Emliy',
-    	email: 	'emliy@gmail.com',
-    	number: '(222) 222-2222'
-    };
-    person3 = {
-    	name: 	'John',
-    	email: 	'john@gmail.com',
-    	number: '(333) 333-3333'
-    };
 
-    var contactlist = [person1,person2,person3];
+    $http.get('/contactlist').success(function(a){
+    	console.log("I got the data I requested");
+    	$scope.contactlist = a;
+    })
 
-    $scope.contactlist = contactlist;
 }]);﻿
--- server.js
+++ server.js
@@ -2,5 +2,30 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+app.get('/contactlist', function(req, res) {
+	console.log("I received a GET request");
+
+    person1 = {
+	name: 	'Tim',
+	email: 	'tim@gmail.com',
+	number: '(111) 111-1111'
+    };
+    person2 = {
+    	name: 	'Emliy',
+    	email: 	'emliy@gmail.com',
+    	number: '(222) 222-2222'
+    };
+    person3 = {
+    	name: 	'John',
+    	email: 	'john@gmail.com',
+    	number: '(333) 333-4444'
+    };
+
+    var contactlist = [person1,person2,person3];
+    res.json(contactlist);
+
+});
+
 app.listen(3000);
 console.log("Server is running on port 3000!");
