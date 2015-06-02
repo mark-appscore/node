2bc5aa16ee4a651f72d9d61502306907ab40f19e
mark-appscore <mark.wang@appscore.com.au>
2015-06-02 22:21:19 +1000

    display contact list by using  method

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,4 +1,23 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+    person1 = {
+    	name: 	'Tim',
+    	email: 	'tim@gmail.com',
+    	number: '(111) 111-1111'
+    };
+    person2 = {
+    	name: 	'Emliy',
+    	email: 	'emliy@gmail.com',
+    	number: '(222) 222-2222'
+    };
+    person3 = {
+    	name: 	'John',
+    	email: 	'john@gmail.com',
+    	number: '(333) 333-3333'
+    };
+
+    var contactlist = [person1,person2,person3];
+
+    $scope.contactlist = contactlist;
 }]);﻿
