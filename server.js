66f4b32eba2a5693fb5307feb386789eb57b114a
mark-appscore <mark.wang@appscore.com.au>
2015-06-03 23:40:54 +1000

    Add contact function

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+
 var app = express();
 var db = mongojs('contactlist',['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res) {
 	console.log("I received a GET request");
@@ -13,6 +16,13 @@ app.get('/contactlist', function(req, res) {
         res.json(docs);
     });
 
+});
+
+app.post('/contactlist',function(req,res){
+    console.log(req.body);
+    db.contactlist.insert(req.body,function(err,doc) {
+        res.json(doc);
+    })
 });
 
 app.listen(3000);
